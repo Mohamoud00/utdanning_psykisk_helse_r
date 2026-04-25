66479d32200a9fe2f8dbca2546a370f661175147
Mohamoud00 <90645446+Mohamoud00@users.noreply.github.com>
2026-04-25 19:53:14 +0200

    Add files via upload

--- 01_import_data.R
+++ 01_import_data.R
@@ -1,6 +1,12 @@
 library(haven)
-data <- read_sav("data/ESS Country data - No.sav")
-names(data)
+
+
+
+data <- read_sav("../data/ESS Country data - No.sav")
+
+
+#Inspeksjon av variabler
+
 str(data)
 grep("edu", names (data), value = TRUE)
 grep("life| happy| depress| health", names(data), value = TRUE)
@@ -21,6 +27,10 @@ attributes(data$eduyrs)
 attributes(data$eduyrs)
 table(data$eduyrs)
 library(dplyr)
+
+
+#Fjerner manglende verdier p?? indeksvariablene jeg ??nsker.
+
 data <- data %>%
   mutate(
     fltdpr = ifelse(fltdpr >= 5, NA, fltdpr) ,
@@ -28,13 +38,25 @@ data <- data %>%
     flteeff = ifelse(flteeff >= 5, NA, flteeff) ,
     fltlnl = ifelse(fltlnl >= 5, NA, fltlnl) 
   )
+
+
+#Sjekker for manglende verdier i indeksvariabler. Stort frafall.  
+
 table(data$fltdpr, useNA = "always")
 sum(is.na(data$fltdpr) | is.na(data$fltanx) | is.na(data$flteeff) | is.na(data$fltlnl))
+
+
+#Konstruksjon av psykisk helseindeks.
+
 data <- data %>%
   mutate(mental_health_index = rowMeans(
     select(., fltdpr, fltanx, flteeff, fltlnl) ,
     na.rm = TRUE
   ))
+
+
+#Sjekker data for konstruert indeks.
+
 summary(data$mental_health_index)
 sum(!is.na(data$mental_health_index))
 sapply(list(fltdpr = data$fltdpr, 
@@ -42,25 +64,23 @@ sapply(list(fltdpr = data$fltdpr,
             flteeff = data$flteeff, 
             fltlnl = data$fltlnl), 
        function(x) sum(is.na(x)))
+
+
+#Cronbach's alfa p?? psykisk helseindeks
+
+# library(psych)
+# install.packages("psych")
+library(psych)
+alpha(data[ c("fltdpr", "fltanx", "flteeff", "fltlnl")])
+
+
+#Deskriptiv statistikk
+
+
 library(ggplot2)
   
-  summary_stats <- data.frame(
-  Statistikk = c("Minimum", "Q1", "Median", "Gjennomsnitt", "Q3", "Maksimum") ,
-  Verdi = c(1.000, 1.000, 1.250, 1.289, 1.500, 4.000)
-)
+#Histogram av fordelingen av respondenter p?? antall utdannings??r- histogram
 
-summary_stats$Statistikk <- factor(summary_stats$Statistikk, 
-                                   levels = c("Minimum", "Q1", "Median", "Gjennomsnitt", "Q3", "Maksimum"))
-ggplot(summary_stats, aes(x = Statistikk, y = Verdi, fill = Statistikk)) +
-  geom_bar(stat = "identity") +
-  theme_minimal() +
-  labs(
-    title = "Summary statistikk - Psykisk helseindeks",
-    x = "",
-    y = "Verdi",
-    caption = "Data: ESS Round 11, Norge"
-  ) +
-  theme(legend.position = "none")
 ggplot(data, aes(x = eduyrs)) +
   geom_histogram(binwidth = 1, fill = "steelblue", color = "white") +
   theme_minimal() +
@@ -70,24 +90,32 @@ ggplot(data, aes(x = eduyrs)) +
     y = "Frekvens",
     caption = "Data: ESS Round 11, Norge"
   )
-sum(is.na(data$eduyrs))
-cor.test(data$eduyrs, data$mental_health_index, use = "complete.obs")
-ggplot(data, aes(x = eduyrs, y = stflife)) +
-  geom_point(alpha = 0.2, color = "steelblue") +
-  geom_smooth(method = "lm", color = "red", se = TRUE) +
+
+
+#Historgram av fordelingen av respondenter p?? psykisk helseindeks-frekvens 
+
+ggplot(data, aes(x = mental_health_index)) +
+  geom_histogram(binwidth = 0.25, fill = "steelblue", color = "white") +
   theme_minimal() +
   labs(
-    title = "Sammenheng mellom utdanningsar og livstilfredshet",
-    x = "Utdanningsar",
-    y = "Livstilfredshet (stflife)",
-    caption = "Data: ESS Round 11, Norge | r = -0.02, p = 0.02, N = 17402"
+    title = "Fordeling av psykisk helseindeks - Norge" ,
+    x = "Psykisk helseindeks (1-4)" ,
+    y = "Frekvens" , 
+    caption = "Data: ESS round 11, Norge"
   )
-names(data)[grep("stl", names(data))]
-names(data)[grep("life|satis|lsat", names(data), ignore.case = TRUE)]
-attributes(data$stflife)
+
+
+#Fjerner manglende verdier p?? livsstilsfredshet
+
 data <- data %>%
   mutate(stflife = ifelse(stflife >= 77, NA, stflife))
+
+#Korrelasjonstest: utdannings??r -> livsstilsfredshet
+
 cor.test(data$eduyrs, data$stflife, use = "complete.obs")
+
+#OLS regresjon- figur. Korrelasjon: utdannings??r -> livsstilsfredshet  
+
 ggplot(data, aes(x = eduyrs, y = stflife)) +
   geom_point(alpha = 0.2, color = "steelblue") +
   geom_smooth(method = "lm", color = "red", se = TRUE) +
@@ -98,6 +126,9 @@ ggplot(data, aes(x = eduyrs, y = stflife)) +
     y = "Livstilfredshet (0-10)",
     caption = "Data: ESS Round 11, Norge | r = -0.017, p = 0.022, N = 17306"
   )
+
+#OLS regresjon- figur. Korrelasjon: utdannings??r-> psykisk helseindeks 
+
 ggplot(data, aes(x = eduyrs, y = mental_health_index)) +
   geom_point(alpha = 0.2, color = "steelblue") +
   geom_smooth(method = "lm", color = "red", se = TRUE) +
@@ -107,20 +138,6 @@ ggplot(data, aes(x = eduyrs, y = mental_health_index)) +
     x = "Utdanningsar",
     y = "Psykisk helseindeks (1-4)",
     caption = "Data: ESS Round 11, Norge | r = -0.075, p < 0.001, N = 6126"
-  )
-# library(psych)
-# install.packages("psych")
-library(psych)
-alpha(data[ c("fltdpr", "fltanx", "flteeff", "fltlnl")])
-library(ggplot2)
-ggplot(data, aes(x = mental_health_index)) +
-  geom_histogram(binwidth = 0.25, fill = "steelblue", color = "white") +
-  theme_minimal() +
-  labs(
-    title = "Fordeling av psykisk helseindeks - Norge" ,
-    x = "Psykisk helseindeks (1-4)" ,
-    y = "Frekvens" , 
-    caption = "Data: ESS round 11, Norge"
   )
 
            
